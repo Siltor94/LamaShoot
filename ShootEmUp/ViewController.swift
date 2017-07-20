0353c279c2da6ce701740800621f8d8a590827c4
etna <etna@g01-r1p06.local>
2017-07-20 12:48:03 +0200

    Adding collusion detection

--- ShootEmUp/ViewController.swift
+++ ShootEmUp/ViewController.swift
@@ -11,11 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var lama: UIImageView!
+    @IBOutlet var enemies: [UIImageView]!
 
     var animationForLama: UIViewPropertyAnimator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        enemies.forEach{(img: UIImageView) in
+            animateEnnemy(img)
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(self.detectColision), userInfo: nil, repeats: true)
     }
     
     @IBAction func moveTUIButton(_ sender: UIButton) {
@@ -31,6 +37,42 @@ class ViewController: UIViewController {
             }
         })
         animationForLama.startAnimation()
+    }
+    
+    @objc private func detectColision(_ enemy: UIImageView){
+
+            enemies.forEach{(img: UIImageView) in
+                if (lama.layer.presentation()?.frame.intersects((img.layer.presentation()?.frame)!))! {
+                    print("COLLISION")
+                }
+            }
+        }
+
+    private func animateEnnemy(_ img: UIImageView){
+        var t: TimeInterval?
+        switch img.tag {
+        case 1:
+            t = TimeInterval(arc4random_uniform(7 - 4) + 4)
+            break
+        case 2:
+            t = TimeInterval(arc4random_uniform(8 - 3) + 3)
+            break
+        case 3:
+            t = TimeInterval(arc4random_uniform(12 - 8) + 8)
+            break
+        default:
+            ()
+        }
+        
+        if let duration = t {
+            UIView.animate(withDuration: duration, animations: {
+                img.center.y = self.view.frame.size.height + 100
+            }, completion: {
+                (true) in
+                img.center.y = -100
+                self.animateEnnemy(img)
+            })
+        }
     }
 }
 
